f627e2453129fd9b4457696a16a883dc1a6eca68
Alexander Groß <agross@therightstuff.de>
2021-12-08 11:38:54 +0100

    Mapping von 3 teilbaren

--- KataFizzBuzz/Program.cs
+++ KataFizzBuzz/Program.cs
@@ -10,11 +10,11 @@ namespace KataFizzBuzz
 
       // Zahlengenerator
       // Ausgabe bestimmen (n Zahlen -> n Outputs)
-      // Mapping einer Zahl (1 Zahl -> 1 Output)
-      // zahl -> zahl
-      // zahl -> fizz (/3)
-      // zahl -> buzz (/5)
+      // Mapping einer Zahl (1 Zahl -> 1 Output, Reihenfolge der Mapper)
       // zahl -> fizzbuzz (/3 && /5)
+      // zahl -> buzz (/5)
+      // zahl -> fizz (/3)
+      // zahl -> zahl
       // Ausgabe auf der Konsole
     }
   }
--- KataFizzBuzz/ZahlZuFizzMapperTests.cs
+++ KataFizzBuzz/ZahlZuFizzMapperTests.cs
@@ -25,11 +25,39 @@ namespace KataFizzBuzz
     }
   }
 
+  public class Wenn_eine_nicht_durch_3_teilbare_Zahl_gemappt_wird
+  {
+    const int NichtDurch3Teilbar = 1;
+    readonly int _eingabe;
+    readonly string _ausgabe;
+
+    public Wenn_eine_nicht_durch_3_teilbare_Zahl_gemappt_wird()
+    {
+      var mapper = new ZahlZuFizzMapper();
+      _eingabe = NichtDurch3Teilbar;
+
+      _ausgabe = mapper.Übersetzen(_eingabe);
+    }
+
+    [Fact]
+    public void kommt_die_Zahl_als_String_zurück()
+    {
+      _ausgabe.Should().Be(_eingabe.ToString());
+    }
+  }
+
   public class ZahlZuFizzMapper
   {
+    // 3 -> Fizz
+    // 1 -> null (false)
     public string Übersetzen(int eingabe)
     {
-      return "Fizz";
+      if (eingabe % 3 == 0)
+      {
+        return "Fizz";
+      }
+
+      return eingabe.ToString();
     }
   }
 }
