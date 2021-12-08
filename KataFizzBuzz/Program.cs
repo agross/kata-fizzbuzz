a45c597f53ad0606f2ebcb00728c2701d358a7e7
Alexander Groß <agross@therightstuff.de>
2021-12-08 11:14:08 +0100

    Initial commit

---
+++ KataFizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace KataFizzBuzz
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    {
+      // 1, 2, Fizz, 4, Buzz, Fizz...
+
+      // Zahlengenerator
+      // Ausgabe bestimmen (n Zahlen -> n Outputs)
+      // Mapping einer Zahl (1 Zahl -> 1 Output)
+      // Ausgabe auf der Konsole
+    }
+  }
+}
---
+++ KataFizzBuzz/ZahlengeneratorTests.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using FluentAssertions;
+
+using Xunit;
+
+
+// Red, Green, Refactor
+namespace KataFizzBuzz
+{
+  public class Wenn_die_Zahlen_abgerufen_wird
+  {
+    readonly IEnumerable<int> _zahlen;
+
+    // Arrange + Act
+    public Wenn_die_Zahlen_abgerufen_wird()
+    {
+      var generator = new ZahlenGenerator();
+
+      _zahlen = generator.GibMirDieZahlen();
+    }
+
+    [Fact]
+    // NUnit: [Test]
+    public void soll_die_erste_1_sein()
+    {
+      _zahlen.First().Should().Be(1);
+    }
+
+    [Fact]
+    // NUnit: [Test]
+    public void soll_die_letzte_100_sein()
+    {
+      _zahlen.Last().Should().Be(100);
+    }
+
+    [Fact]
+    public void sollen_es_100_Zahlen_sein()
+    {
+      // _zahlen.Count().Should().Be(100);
+      _zahlen.Should().HaveCount(100);
+    }
+  }
+
+  public class ZahlenGenerator
+  {
+    public IEnumerable<int> GibMirDieZahlen()
+      => Enumerable.Range(1, 100);
+  }
+}
