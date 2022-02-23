5a65115b5ebd431aff57f9d9a11714df89b835a9
Alexander Groß <agross@therightstuff.de>
2022-02-23 10:10:27 +0100

    Convert to .NET Core

---
+++ FizzBuzzNetCore/Program.cs
@@ -0,0 +1,10 @@
+﻿// 1, 2, Fizz, 4, Buzz, Fizz...
+
+// Zahlengenerator
+// Ausgabe bestimmen (n Zahlen -> n Outputs)
+// Mapping einer Zahl (1 Zahl -> 1 Output, Reihenfolge der Mapper)
+// zahl -> fizzbuzz (/3 && /5)
+// zahl -> buzz (/5)
+// zahl -> fizz (/3)
+// zahl -> zahl
+// Ausgabe auf der Konsole
